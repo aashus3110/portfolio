3f5cef3812f1ce679d5230397cef6941ef60abd9
ashu3110 <avsingh000.789@gmail.com>
2022-09-28 15:17:56 +0530

    Initial Commit

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,6 +3,9 @@ import './Intro.css'
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
 
 function Intro() {
   return (
@@ -21,9 +24,13 @@ function Intro() {
         </div>
       </div>
       <div className="i-right">
-        i am right side
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={boy} alt="" />
+        <div>
+        </div>
       </div>
-    </div>)
+    </div >)
 }
 
 export default Intro
