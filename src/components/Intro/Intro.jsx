fff6b397bf2507a439f6b7e741946e32035817b1
ashu3110 <avsingh000.789@gmail.com>
2022-10-29 10:02:55 +0530

    Initial Commit

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -6,6 +6,11 @@ import Instagram from "../../img/instagram.png";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+
 
 function Intro() {
   return (
@@ -27,8 +32,23 @@ function Intro() {
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
         <img src={boy} alt="" />
-        <div>
+        <img src={glassesimoji} alt="" />
+
+        <div style={{ top: '-4', left: '68%' }}>
+          <FloatingDiv img={crown} text1="Web" text2="Developer" />
         </div>
+        <div style={{ top: '18rem', left: '0rem' }}>
+          <FloatingDiv img={thumbup} text1="Best Design" text2="Award" />
+        </div>
+        <div className="blur" style={{ background: "rgb(240 200 250)"}}></div>
+        <div className="blur" 
+        style={{ 
+          background: '#C1F5FF',
+          top: '14rem',
+          width: '21rem',
+          height: '11rem',
+          left: '-9rem',
+          }}></div>
       </div>
     </div >)
 }
