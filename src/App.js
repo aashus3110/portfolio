dd10a84d26b5a68e3170b79c7a586d2deb9e3e8b
ashu3110 <avsingh000.789@gmail.com>
2022-12-25 12:15:29 +0530

    EX & work commplit
    EX & work commplit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css'
 import Intro from './components/Intro/Intro'
 import { Navbar } from './components/Navbar/Navbar'
 import Services from './components/Services/Services'
+import Experience from './components/Experience/Experience'
+import Works from './components/Works/Works'
 
 const App = props => {
   return (
@@ -10,6 +12,8 @@ const App = props => {
       <Navbar />
       <Intro />
       <Services />
+      <Experience />
+      <Works />
     </div>
   )
 }
